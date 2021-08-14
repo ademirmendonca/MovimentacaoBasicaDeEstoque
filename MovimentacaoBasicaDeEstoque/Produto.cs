0b2594d13ff7c160d4b3fc0c467d0069380977c3
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-08-14 15:04:32 -0300

    Add project files.

---
+++ MovimentacaoBasicaDeEstoque/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace MovimentacaoBasicaDeEstoque
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        //Definição de como um produto será transformado em forma de String
+        public override string ToString()
+        {
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", "
+                + Quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2",CultureInfo.InvariantCulture);
+
+        }
+    }
+}
